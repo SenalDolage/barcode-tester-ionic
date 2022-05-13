8329288463a76c61ac025d8237c56109ed85e2a5
Senal Dolage <senal.dolage@gmail.com>
2022-05-13 17:36:05 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { throwError } from 'rxjs';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
-  constructor() {}
+  async scanDoor() {
+    // if (!environment.production) {
+    //   return (window && window['doornr']) || '2.6.Wc';
+    // }
 
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+        return barcodeData.text.toString();
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+
+    // const barcodeData = await this.barcodeScanner.scan();
+    // if (barcodeData.text.toString().trim() !== '') {
+    //   console.log(barcodeData.text.toString());
+    //   return barcodeData.text.toString();
+    // } else {
+    //   throwError('QR code scan result invalid');
+    // }
+  }
 }
